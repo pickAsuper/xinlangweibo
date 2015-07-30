a53dfdad0b4392ac502e10ad59267153029ae155
manager <qwe@126.com>
2015-07-31 00:39:39 +0800

    更改登录界面...待续..

---
+++ 新浪微博项目/新浪微博/新浪微博/Classes/Public/Category/UIWindow+Extetion.h
@@ -0,0 +1,14 @@
+//
+//  UIWindow+Extetion.h
+//  新浪微博
+//
+//  Created by lichao on 15/7/31.
+//  Copyright (c) 2015年 ___Super___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (Extetion)
++(void)switchRootViewCtrl;
+
+@end
---
+++ 新浪微博项目/新浪微博/新浪微博/Classes/Public/Category/UIWindow+Extetion.m
@@ -0,0 +1,20 @@
+//
+//  UIWindow+Extetion.m
+//  新浪微博
+//
+//  Created by lichao on 15/7/31.
+//  Copyright (c) 2015年 ___Super___. All rights reserved.
+//
+
+#import "UIWindow+Extetion.h"
+
+@implementation UIWindow (Extetion)
+
++(void)switchRootViewCtrl{
+
+    
+
+}
+
+
+@end
